15a2c574ce51c62b13472e90547f77a3e1b0a9e4
lesterdeguzman <32890425+lesterdeguzman@users.noreply.github.com>
2019-04-28 13:04:09 +0800

    Update report_admin.php

--- php/report_admin.php
+++ php/report_admin.php
@@ -43,16 +43,20 @@
     
  <?php
 
-  $sql = "SELECT * FROM reservation WHERE status LIKE 'Approved'";
+  $sql = "SELECT * FROM report";
   $result = $conn-> query($sql);
 
   if ($result-> num_rows > 0){
     while ($row = $result-> fetch_assoc()){
       echo "<tr>
-          <td>" . $row["title"] . "</td>
-          <td>" . $row["Venue"] . "</td>          
-          <td>"  . $row["start_event"] . "</td>
-          <td>" . $row["end_event"] . "</td>          
+          <td>" . $row["DateOfOccurence"] . "</td>
+          <td>" . $row["TimeOfOccurence"] . "</td>          
+          <td>"  . $row["Name"] . "</td>
+          <td>" . $row["IdNo"] . "</td>
+          <td>" . $row["Course"] . "</td>
+          <td>" . $row["Description"] . "</td>
+          <td>" . $row["ExtentofDamage"] . "</td> 
+          <td>" . $row["Action_taken"] . "</td>             
           </tr>";
     }
     echo "</table>";
