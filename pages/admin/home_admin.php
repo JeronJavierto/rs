66a78a4398f6b5f7fc4b14e5c8c699bede1a8d2a
lesterdeguzman <32890425+lesterdeguzman@users.noreply.github.com>
2019-04-28 13:04:01 +0800

    Update home_admin.php

--- pages/admin/home_admin.php
+++ pages/admin/home_admin.php
@@ -18,7 +18,7 @@
 		<li class="events"><a href="../../php/event_page_admin.php" class="events">EVENTS</a></li>
 		<li class="faci"><a href="../../php/list_facilities_admin.php" class="faci">FACILITIES</a></li>
 		<li class="req"><a href="../../php/list_of_requests_admin.php" class="req">REQUEST</a></li>
-		<li class="rep"><a href="../../admin/report.php" class="rep">REPORTS</a></li>
+		<li class="rep"><a href="../../php/report_admin.php" class="rep">REPORTS</a></li>
 	</ul>
 
 	<?php include("homepage_admin_calendar.php"); ?>
