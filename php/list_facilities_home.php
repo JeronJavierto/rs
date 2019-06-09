ffe3829d191112c38bd73a6408ff377c90ddfe9b
yeza mae <2165093@slu.edu.ph>
2019-06-09 16:31:13 +0800

    REMOVED signup

--- php/list_facilities_home.php
+++ php/list_facilities_home.php
@@ -141,7 +141,6 @@ if (isset($_GET['page_no']) && $_GET['page_no']!="") {
 </div>
 
 <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;" class="loginB">LOGIN</button>
-  <button onclick="document.getElementById('id02').style.display='block'" style="width:auto;" class="signupB">SIGN UP</button>
 
   <div id="id01" class="loginmodal">
   <form class="modal-content animate" action="login.php" method="POST">
@@ -161,90 +160,6 @@ if (isset($_GET['page_no']) && $_GET['page_no']!="") {
     </div>
   </form>
 </div>
-
-<div id="id02" class="modal_signup">
-      <span onclick="document.getElementById('id02').style.display='none'" class="close_s" title="Close">&times;</span>   
-    
-<form class="modal-content-signup" action="signup.php" method="POST">
-    <div class="container_signup">
-      <p>Sign Up</p>
-      <div class="signup-body">
-      <div class="client">
-      <button type="button" onclick="document.getElementById('id03').style.display='block'" class="clientbtn">CLIENT</button>  
-      </div>
-      <div class="admin">
-      <button type="button" onclick="document.getElementById('id04').style.display='block'" class="adminbtn">ADMIN</button>  
-    </div>
-    </div>
-  </form>
-</div>
-
-      <div id="id03" class="modal_signup">
-          <span onclick="document.getElementById('id03').style.display='none'" class="close_s" title="Close">&times;</span>
-        <form class="modal-content-signup" action="signup.php" method="POST">
-          <div class="container_signup">
-          <p>Please fill in this form to create an account.</p>
-          <hr>
-      <label for="frstn">First Name</label>
-      <input type="text" placeholder="Enter First Name" name="FName" required>
-
-      <label for="lstn">Last Name</label>
-      <input type="text" placeholder="Enter Last Name" name="LName">
-
-      <label for="email"><b>Email</b></label>
-      <input type="text" placeholder="Enter Email" name="email" required>      
-
-      <label for="org">Organization</label>
-      <input type="text" placeholder="Organization" name="organization" required="">
-
-      <label for="posi">Position</label>
-      <input type="text" placeholder="Position" name="position" required="">
-
-      <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
-
-      <label for="psw-repeat"><b>Repeat Password</b></label>
-      <input type="password" placeholder="Repeat Password" name="ConPassword" required>     
-
-      <div class="clearfix_signup">
-        <button type="button" onclick="document.getElementById('id03').style.display='none'" class="cancelbtn_signup">Cancel</button>
-        <button type="submit" class="signupbtn">Sign Up</button>
-      </div>
-        </div>
-        </form>
-      </div>
-
-      <div id="id04" class="modal_signup">
-          <span onclick="document.getElementById('id04').style.display='none'" class="close_s" title="Close">&times;</span>
-        <form class="modal-content-signup" action="signup.php" method="POST">
-          <div class="container_signup">
-          <p>Please fill in this form to create an account.</p>
-          <hr>
-      <label for="frstn">First Name</label>
-      <input type="text" placeholder="Enter First Name" name="FName" required>
-
-      <label for="lstn">Last Name</label>
-      <input type="text" placeholder="Enter Last Name" name="LName">
-
-      <label for="email"><b>Email</b></label>
-      <input type="text" placeholder="Enter Email" name="email" required>      
-
-      <label for="org">Office</label>
-      <input type="text" placeholder="Organization" name="organization" required="">
-
-      <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
-
-      <label for="psw-repeat"><b>Repeat Password</b></label>
-      <input type="password" placeholder="Repeat Password" name="ConPassword" required>
-      
-      <div class="clearfix_signup">
-        <button type="button" onclick="document.getElementById('id04').style.display='none'" class="cancelbtn_signup">Cancel</button>
-        <button type="submit" class="signupbtn">Sign Up</button>
-      </div>
-        </div>
-        </form>
-      </div>
 
 </body>
 </html>
