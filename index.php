a44fead5825fc77f291002db162fb66232ce8bca
yeza mae <2165093@slu.edu.ph>
2019-07-04 19:08:26 +0800

    logo

--- index.php
+++ index.php
@@ -4,7 +4,12 @@
   <title>Home</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="stylesheet/style.css">
+  <br>
+  <br>
   <h1>FACILITY RESERVATION SYSTEM</h1>
+  <a href="index.php">
+    <img src="images/final logo.png" alt="final-logo">
+  </a>
 </head>
 
 <body>
