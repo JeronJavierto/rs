8ad815b6c7668952202777717b3ef709082a7c34
JeronJavierto <37394437+JeronJavierto@users.noreply.github.com>
2019-07-06 11:30:04 +0800

    old calendar

--- pages/office/homepage_calendar.php
+++ pages/office/homepage_calendar.php
@@ -1,7 +1,14 @@
+<?php
+//index.php
+
+
+
+
+?>
 <!DOCTYPE html>
 <html>
  <head>
-  <title>CALENDAR</title>
+  <title></title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -12,26 +19,39 @@
    
   $(document).ready(function() {
    var calendar = $('#calendar').fullCalendar({
-    editable:false,
+    eventLimit: true,
+    navLinks: true,
+    nowIndicator: true,
+    windowResize: function(view) {
+    },
+    businessHours: {          
+        daysOfWeek: [ 1, 2, 3, 4, 5, 6 ], 
+        startTime: '8:00', 
+        endTime: '18:00', 
+    },
+    editable:true,
     header:{
      left:'prev,next today',
      center:'title',
      right:'month,agendaWeek,agendaDay'
-    },
+    },    
     events: 'load_calendar.php',
-    selectable:false,
-    selectHelper:false,
+    selectable:true,
+    selectHelper:true,
     select: function(start, end, allDay)
     {
-     var title = prompt("Enter Event Title");
-     if(title)
+     var title = prompt("Enter Event Name:");
+     var facility = prompt("Facility: ");
+     var participants = prompt("Estimated participants: ");
+
+     if(title && facility && participants)
      {
-      var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
-      var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
+      var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm");
+      var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm");
       $.ajax({
        url:"insert_calendar.php",
        type:"POST",
-       data:{title:title, start:start, end:end},
+       data:{title:title, facility:facility, participants:participants, start:start, end:end},
        success:function()
        {
         calendar.fullCalendar('refetchEvents');
@@ -40,11 +60,11 @@
       })
      }
     },
-    editable:false,
+    editable:true,
     eventResize:function(event)
     {
-     var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
-     var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+     var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm");
+     var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm");
      var title = event.title;
      var id = event.id;
      $.ajax({
@@ -57,56 +77,16 @@
       }
      })
     },
-
-    eventDrop:function(event)
-    {
-     var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
-     var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
-     var title = event.title;
-     var id = event.id;
-     $.ajax({
-      url:"update_calendar.php",
-      type:"POST",
-      data:{title:title, start:start, end:end, id:id},
-      success:function()
-      {
-       calendar.fullCalendar('refetchEvents');
-       alert("Event Updated");
-      }
-     });
-    },
-
-    eventClick:function(event)
-    {
-     if(confirm("Are you sure you want to remove it?"))
-     {
-      var id = event.id;
-      $.ajax({
-       url:"delete_calendar.php",
-       type:"POST",
-       data:{id:id},
-       success:function()
-       {
-        calendar.fullCalendar('refetchEvents');
-        alert("Event Removed");
-       }
-      })
-     }
-    },
-
    });
   });
    
   </script>
  </head>
  <body>
-  <br />
-  <h2 align="center"><a href="#">calendar</a></h2>
+  <br />  
   <br />
   <div class="container">
    <div id="calendar"></div>
   </div>
  </body>
 </html>
-
-?>
